23718928ba357e2188a49cbaff768658a70e9f25
Marlon Costa <marlon.costa@ieee.org>
2017-08-20 17:28:23 -0300

    Agrupamento de dados juntando nome do deputado ao fornecedor
    TODO: Somar corretamente os valores

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from networkx import *
 import pandas
 
 df = pandas.read_csv("Ano-2017.csv", sep = ";", header = 0)
-df = df[df['sgUF'] == 'PB']
-print (df)
+df = df[['NomeParlamentar','sgUF','vlrDocumento','Fornecedor']]
+df = df[df['sgUF']== 'PB']
+df = df.groupby(['NomeParlamentar', 'Fornecedor'])['vlrDocumento'].sum(axis= 1, numeric_only = True)
+print(df)
