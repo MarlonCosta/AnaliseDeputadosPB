451b9ae4d2a273402a6d5acf65df570658502991
Marlon Costa <marlon.costa@ieee.org>
2017-08-22 23:30:38 -0300

    - Substituição de vírgulas por pontos no csv fonte.
    - Ajustes da saída

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ import pandas
 df = pandas.read_csv("Ano-2017.csv", sep = ";", header = 0)
 
 # Mantém apenas as colunas descritas como argumento
-df = df[['NomeParlamentar','sgUF','vlrDocumento','Fornecedor']]
+df = df[['NomeParlamentar','CNPJCPF','sgUF','vlrDocumento','Fornecedor']]
 
 #Separa apenas os deputados da PB
 df = df[df['sgUF']== 'PB']
 
+df['vlrDocumento'] = df['vlrDocumento'].astype(float)
+
 #Agrupa os valores por Nome do Parlamentar e do Fornecedor
-df = df.groupby(['NomeParlamentar', 'Fornecedor'])['vlrDocumento'].sum(axis= 1, numeric_only = True)
+
+df = df.groupby(['NomeParlamentar', 'CNPJCPF'])['vlrDocumento'].sum()
+
+#Converte o groupby novamente pra DataFrame
+df = pandas.DataFrame(df)
+
+#Exporta o resultado
+df.to_csv(path_or_buf='resultado.csv', sep=';', header=True)
 
 #Advinha
 print(df)
