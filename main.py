ab266c9eeb9e603ea399ade1a7d91f9a3a4df986
Marlon Costa <marlon.costa@ieee.org>
2017-08-29 01:42:27 -0300

    Otimizações no código baseadas no do projeto-base

--- main.py
+++ main.py
@@ -1,30 +1,45 @@
+import urllib
+from bs4 import BeautifulSoup
 from networkx import *
 import pandas
+from unicodedata import normalize
+
+page = urllib.request.urlopen(
+    "http://www.camara.leg.br/SitCamaraWS/Deputados.asmx/ObterDeputados")  # link com os dados básicos dos deputados
+soup = BeautifulSoup(page, "lxml")
+lista_dados_deputados = soup.find_all("deputado")
+
+
+def remover_acentos(txt):
+    return normalize("NFKD", txt).encode("ASCII", "ignore").decode("ASCII")
+
+
+deputados_dict = {}
+for i in range(0, len(lista_dados_deputados)):
+    deputados_dict[lista_dados_deputados[i].idecadastro.string] = [
+        remover_acentos(lista_dados_deputados[i].nome.string), lista_dados_deputados[i].urlfoto.string]
 
 # Abre o CSV
-df = pandas.read_csv("Ano-2017.csv", sep = ";", header = 0)
+df_cota = pandas.read_csv("cota/Ano-2016.csv", sep=";", decimal=',', header=0)
 
-# Mantém apenas as colunas descritas como argumento
-df = df[['NomeParlamentar','CNPJCPF','sgUF','vlrDocumento','Fornecedor']]
+# Separa apenas os deputados da PB
+df_cota = df_cota[df_cota['sgUF'] == 'PB']
 
-#Separa apenas os deputados da PB
-df = df[df['sgUF']== 'PB']
+# Remove os valores nulos (sem CNPJ/CPF ou sem valor)
+df_cota = df_cota[df_cota.txtCNPJCPF.notnull()]
+df_cota = df_cota[df_cota.vlrLiquido.notnull()]
 
-#Converte a coluna de valores pra float
-df['vlrDocumento'] = df['vlrDocumento'].astype(float)
+# Remove a notação científica do CNPJ/CPF
+df_cota['txtCNPJCPF'] = (df_cota['txtCNPJCPF'].map(lambda x: '{:.0f}'.format(x)))
 
-#Remoção da notação científica da coluna de CNPJ/CPF
-df['CNPJCPF'] = (df['CNPJCPF'].map(lambda x: '{:.0f}'.format(x)))
+df_cota_resumo = df_cota.groupby(["txNomeParlamentar", "sgPartido", "sgUF", "txtCNPJCPF", "txtFornecedor"]).agg(
+    {"vlrLiquido": ["sum", "count"]}).reset_index()
 
-#Agrupa os valores por Nome do Parlamentar e do Fornecedor
+# Advinha
+print(df_cota_resumo)
 
-df = df.groupby(['NomeParlamentar', 'CNPJCPF'])['vlrDocumento'].sum()
+# Criando grafo
+G = nx.from_pandas_dataframe(df_cota, "txNomeParlamentar", 'txtCNPJCPF', create_using=nx.DiGraph())
 
-#Converte o groupby novamente pra DataFrame
-df = pandas.DataFrame(df)
-
-#Exporta o resultado
-df.to_csv(path_or_buf='resultado.csv', sep=';', header=True)
-
-#Advinha
-print(df)
+# Testando se cada parlamentar e empresa virou um nó
+print(G.nodes())
